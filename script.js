1d71e9e145483856aacd6ea17e089c30aa2c3fa9
Jens <jens.poldme@gmail.com>
2017-09-21 15:22:04 +0300

    script update

--- script.js
+++ script.js
@@ -1,48 +1,40 @@
 
+//VARIABLES
 const buttons = document.querySelectorAll('.button');
 const strictMode = document.querySelector('button');
 const controls = document.querySelector('.gameControl');
 const colors = {0:'green', 1:'red', 2:'yellow', 3:'blue'};
 const colorHighlights = {'green':'#8CD538', 'red':'#f3462c', 'yellow':'#fdf04f', 'blue':'#0ab3fb'};
-const trueColors = {'green':'#629935', 'red':'#be332a', 'yellow':'#e2d445', 'blue':'#0274a5'}
-let playerResponse;
-let gameSequence;
+const trueColors = {'green':'#629935', 'red':'#be332a', 'yellow':'#e2d445', 'blue':'#0274a5'};
+let playerResponse = [];
+let gameSequence = [];
 let turnCount;
 let strict = false;
+let gameOver = false;
 
 
 //FUNCTION DECLRATIONS:
-
 function startGame() {
   turnCount = 1;
+  makeTurn(turnCount);
+}
 
-
-
-
-  /*gameSequence = [];
-  playerResponse = [];
-  //generate new game sequence for this turn.
-  gameSequence = generateGameSequence(turnCount);
-  console.log(gameSequence); //<-- to be removed
-  presentGameSequence(gameSequence);
-  //gather players button sequence.
-  buttons.forEach((button) => {
-    button.addEventListener('click', turnClick);
-  });*/
-  while (turnCount<7) {
-    setTimeout(test.bind(null, turnCount), 750*turnCount);
-    turnCount+=1;
+function makeTurn(count) {
+  if (gameOver) {
+    setTimeout(() => {console.log('game over')}, 1000*count);
+  } else if (count == 7) {
+    gameOver = true;
+  } else {
+    setTimeout(() => {
+      gameSequence = generateGameSequence(count);
+      console.log(gameSequence+' '+count);
+      //presentGameSequence(gameSequence);
+    }, 1000*count);
+    //presentgamesequence
+    //playerResponse = getPlayerResponse
+    makeTurn(count+1);
   }
-
 }
-
-//TESTFUNC
-function test(count) {
-  console.log('turn: '+count);
-}
-//TESTFUNC
-
-
 
 //generate a random sequence of button values depending on turnCount.
 function generateGameSequence(count) {
@@ -54,13 +46,14 @@ function presentGameSequence(array) {
     setTimeout(presentColor.bind(null, color), 1000*i);
   })
 }
-//blinks the color button
+//blinks the color button.
 function presentColor(color) {
   let colorButton = document.getElementById(color);
   colorButton.style.background = colorHighlights[color];
   setTimeout(() => {colorButton.style.background = trueColors[color]}, 750);
 }
 
+//////////////////////////////////////////////////////////////////////////////
 function turnClick(button) {
   let targetValue = button.target.id.toString();
   (evaluateState(targetValue)) ? rightQuess(targetValue) : wrongQuess(strict);
@@ -75,16 +68,15 @@ function evaluateState(value) {
 }
 function rightQuess(value) {
   playerResponse.push(value)
-  console.log(playerResponse); //<-------------remove
   if (gameSequence.length === playerResponse.length) console.log('you win');
 }
 function wrongQuess(strict) {
   (strict) ? console.log('game over') : console.log('false'); //<------- !!!
 }
-
+//////////////////////////////////////////////////////////////////////////////
 
 //FUNCTION INITIATION:
-strictMode.addEventListener("click", function(){
+/*strictMode.addEventListener("click", function(){
     if(this.innerText === 'OFF') {
       this.innerText = 'ON';
       strict = true;
@@ -94,6 +86,8 @@ strictMode.addEventListener("click", function(){
       strict = false;
       this.style.background = '#6faf3b';
     }
-});
+});*/
 
-startGame();
+controls.addEventListener('click', function() {
+  startGame();
+})
--- test.js
+++ test.js
@@ -6,6 +6,53 @@ function startGame() {
     presentGameSequence(gameSequence);
     playerResponse = getPlayerResponse();
     evaluateResponse(gameSequence, playerResponse);
-    count +=1;
+    makeTurn(turnCount+1);
   }
 }
+
+
+//github
+    const colors = {0:'green', 1:'red', 2:'yellow', 3:'blue'};
+    const colorHighlights = {'green':'#8CD538', 'red':'#f3462c', 'yellow':'#fdf04f', 'blue':'#0ab3fb'};
+    const trueColors = {'green':'#629935', 'red':'#be332a', 'yellow':'#e2d445', 'blue':'#0274a5'}
+    let gameSequence = [];
+    let turnCount;
+    let gameOver = false;
+
+    function startGame() {
+      turnCount = 1;
+      makeTurn(turnCount);
+    }
+
+    function makeTurn(count) {
+      if (gameOver) {
+        setTimeout(() => {console.log('game over')}, 3000*count);
+      } else if (count == 7) {
+        gameOver = true;
+      } else {
+        setTimeout(() => {
+          gameSequence = generateGameSequence(count);
+          console.log(gameSequence+' '+count);
+          presentGameSequence(gameSequence);
+        }, 3000*count);
+        makeTurn(count+1);
+      }
+    }
+
+    //generate a random sequence of button values depending on turnCount.
+    function generateGameSequence(count) {
+      return Array.from({length: count}, () => colors[Math.floor(Math.random()*4)]);
+    }
+
+    //presents the colors in the gameSequence on the game board.
+    function presentGameSequence(array) {
+      array.forEach((color, i) => {
+        setTimeout(presentColor.bind(null, color), 1000*i);
+      })
+    }
+    //blinks the color button
+    function presentColor(color) {
+      let colorButton = document.getElementById(color);
+      colorButton.style.background = colorHighlights[color];
+      setTimeout(() => {colorButton.style.background = trueColors[color]}, 750);
+    }
